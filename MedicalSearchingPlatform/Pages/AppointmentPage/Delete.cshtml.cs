a380be1d815e84bc1d5f075ea7a3bdc4ccc37ab3
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-03-27 01:08:21 +0700

    make appointment

---
+++ MedicalSearchingPlatform/Pages/AppointmentPage/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MedicalSearchingPlatform.Data.DataContext;
+using MedicalSearchingPlatform.Data.Entities;
+
+namespace MedicalSearchingPlatform.Pages.AppointmentPage
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext _context;
+
+        public DeleteModel(MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Appointment Appointment { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FirstOrDefaultAsync(m => m.AppointmentId == id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Appointment = appointment;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                Appointment = appointment;
+                _context.Appointments.Remove(Appointment);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ MedicalSearchingPlatform/Pages/AppointmentPage/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MedicalSearchingPlatform.Data.DataContext;
+using MedicalSearchingPlatform.Data.Entities;
+
+namespace MedicalSearchingPlatform.Pages.AppointmentPage
+{
+    public class EditModel : PageModel
+    {
+        private readonly MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext _context;
+
+        public EditModel(MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Appointment Appointment { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment =  await _context.Appointments.FirstOrDefaultAsync(m => m.AppointmentId == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            Appointment = appointment;
+           ViewData["DoctorId"] = new SelectList(_context.Doctors, "DoctorId", "DoctorId");
+           ViewData["PatientId"] = new SelectList(_context.Patients, "PatientId", "PatientId");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Appointment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(Appointment.AppointmentId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool AppointmentExists(string id)
+        {
+            return _context.Appointments.Any(e => e.AppointmentId == id);
+        }
+    }
+}
