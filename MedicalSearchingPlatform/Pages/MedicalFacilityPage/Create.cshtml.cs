e6da4814122f06c22e122f690f8d898bb6b7ec51
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-03-26 22:55:16 +0700

    facility route

---
+++ MedicalSearchingPlatform/Pages/MedicalFacilityPage/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MedicalSearchingPlatform.Data.DataContext;
+using MedicalSearchingPlatform.Data.Entities;
+
+namespace MedicalSearchingPlatform.Pages.MedicalFacilityPage
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext _context;
+
+        public CreateModel(MedicalSearchingPlatform.Data.DataContext.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public MedicalFacility MedicalFacility { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.MedicalFacilities.Add(MedicalFacility);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
