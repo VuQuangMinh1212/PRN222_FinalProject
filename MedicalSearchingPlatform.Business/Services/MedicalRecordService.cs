d431806589c5e0cbc167d980987713f3fd6d7337
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-03-31 01:07:44 +0700

    add back service

---
+++ MedicalSearchingPlatform.Business/Services/MedicalRecordService.cs
@@ -0,0 +1,67 @@
+﻿using MedicalSearchingPlatform.Data.Entities;
+using MedicalSearchingPlatform.Data.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MedicalSearchingPlatform.Services
+{
+    public class MedicalRecordService : IMedicalRecordService
+    {
+        private readonly IMedicalRecordRepository _medicalRecordRepository;
+
+        public MedicalRecordService(IMedicalRecordRepository medicalRecordRepository)
+        {
+            _medicalRecordRepository = medicalRecordRepository;
+        }
+
+        public async Task<MedicalRecord> GetMedicalRecordAsync(string medicalRecordId)
+        {
+            return await _medicalRecordRepository.GetByIdAsync(medicalRecordId);
+        }
+
+        public async Task<IEnumerable<MedicalRecord>> GetPatientRecordsAsync(string patientId)
+        {
+            return await _medicalRecordRepository.GetByPatientIdAsync(patientId);
+        }
+
+        public async Task<IEnumerable<MedicalRecord>> GetDoctorRecordsAsync(string doctorId)
+        {
+            return await _medicalRecordRepository.GetByDoctorIdAsync(doctorId);
+        }
+
+        public async Task<MedicalRecord> CreateMedicalRecordAsync(MedicalRecord medicalRecord)
+        {
+            return await _medicalRecordRepository.AddAsync(medicalRecord);
+        }
+
+        public async Task UpdateMedicalRecordAsync(MedicalRecord medicalRecord)
+        {
+            await _medicalRecordRepository.UpdateAsync(medicalRecord);
+        }
+
+        public async Task DeleteMedicalRecordAsync(string medicalRecordId)
+        {
+            await _medicalRecordRepository.DeleteAsync(medicalRecordId);
+        }
+
+        public async Task ShareRecordWithDoctorAsync(string medicalRecordId, bool isShared)
+        {
+            var record = await _medicalRecordRepository.GetByIdAsync(medicalRecordId);
+            if (record != null)
+            {
+                record.IsShared = isShared;
+                await _medicalRecordRepository.UpdateAsync(record);
+            }
+        }
+
+        public async Task UploadAttachmentAsync(string medicalRecordId, string attachmentUrl)
+        {
+            var record = await _medicalRecordRepository.GetByIdAsync(medicalRecordId);
+            if (record != null)
+            {
+                record.AttachmentUrl = attachmentUrl;
+                await _medicalRecordRepository.UpdateAsync(record);
+            }
+        }
+    }
+}
